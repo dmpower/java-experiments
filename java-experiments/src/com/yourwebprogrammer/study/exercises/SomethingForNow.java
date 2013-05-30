386affc57d368f9c4346c6bcae2b663597b14d48
dmpower <dpower@YourWebProgrammer.com>
2013-05-29 22:58:56 -0700

    New stuff.

---
+++ java-experiments/src/com/yourwebprogrammer/study/exercises/SomethingForNow.java
@@ -0,0 +1,13 @@
+package com.yourwebprogrammer.study.exercises;
+
+public class SomethingForNow {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
